68280b7a861f1cc8cb9cc81971a41949d4e5a23a
funnsam <funnsam8@gmail.com>
2024-04-18 16:04:42 +0000

    alternate ϕ

--- src/traits.rs
+++ src/traits.rs
@@ -14,7 +14,7 @@ impl FromConstant for f32 {
     fn from_constant(c: char) -> Option<Self> {
         match c {
             'π' => Some(std::f32::consts::PI),
-            'φ' => Some(1.61803398874989484820),
+            'φ' | 'ϕ' => Some(1.61803398874989484820),
             'e' => Some(std::f32::consts::E),
             'τ' => Some(std::f32::consts::TAU),
             'γ' => Some(0.57721566490153286060),
@@ -27,7 +27,7 @@ impl FromConstant for f64 {
     fn from_constant(c: char) -> Option<Self> {
         match c {
             'π' => Some(std::f64::consts::PI),
-            'φ' => Some(1.61803398874989484820),
+            'φ' | 'ϕ' => Some(1.61803398874989484820),
             'e' => Some(std::f64::consts::E),
             'τ' => Some(std::f64::consts::TAU),
             'γ' => Some(0.57721566490153286060),
