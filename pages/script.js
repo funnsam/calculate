b0f6d30fe6f9e2e46afd0e11a89bee170bcf66f3
funnsam <funnsam8@gmail.com>
2024-04-22 22:51:10 +0000

    switchable types

--- pages/script.js
+++ pages/script.js
@@ -1,4 +1,15 @@
-import init, { evaluate, JsSpan } from "./pkg/bindings.js";
+import init, { evaluate_f32, evaluate_f64, evaluate_rational, JsSpan } from "./pkg/bindings.js";
+
+let evaluate = evaluate_rational;
+
+const urlp = new URLSearchParams(window.location.search);
+const type = urlp.get("type");
+
+if (type == "f32") {
+	evaluate = (v) => { +evaluate_f32(v).toFixed(5) };
+} else if (type == "f64") {
+	evaluate = (v) => { +evaluate_f64(v).toFixed(5) };
+}
 
 document.addEventListener("DOMContentLoaded", (_) => {
 	function update() {
--- pages/src/lib.rs
+++ pages/src/lib.rs
@@ -1,4 +1,7 @@
-use calculate::*;
+use calculate::{*, traits::*};
+use num_bigint::BigInt;
+use num_rational::BigRational;
+use num_traits::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -8,11 +11,151 @@ pub struct JsSpan {
 }
 
 #[wasm_bindgen]
-pub fn evaluate(s: &str) -> Result<f64, JsSpan> {
+pub fn evaluate_f32(s: &str) -> Result<f32, JsSpan> {
     Ok(to_nodes(s)
         .map_err(|s| JsSpan {
             start: s.start,
             end: s.end,
         })?
         .evaluate())
+}
+
+#[wasm_bindgen]
+pub fn evaluate_f64(s: &str) -> Result<f64, JsSpan> {
+    Ok(to_nodes(s)
+        .map_err(|s| JsSpan {
+            start: s.start,
+            end: s.end,
+        })?
+        .evaluate())
+}
+
+#[wasm_bindgen]
+pub fn evaluate_rational(s: &str) -> Result<String, JsSpan> {
+    Ok(to_nodes::<Rat>(s)
+        .map_err(|s| JsSpan {
+            start: s.start,
+            end: s.end,
+        })?
+        .evaluate()
+        .to_string())
+}
+
+use core::ops::*;
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct Rat(BigRational);
+
+impl std::str::FromStr for Rat {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, ()> {
+        let mut numer = BigInt::from(0);
+        let mut denom = BigInt::from(1);
+        let mut di = false;
+
+        for c in s.chars() {
+            match (c, di) {
+                ('0'..='9', _) => {
+                    numer *= 10;
+                    numer += c as u8 - '0' as u8;
+
+                    if di {
+                        denom *= 10;
+                    }
+                },
+                ('.', false) => di = true,
+                _ => return Err(()),
+            }
+        }
+
+        Ok(Self(BigRational::new(numer, denom)))
+    }
+}
+
+impl FromConstant for Rat {
+    fn from_constant(c: char) -> Option<Self> {
+        None
+    }
+}
+
+macro_rules! delegate_biop {
+    ($t: path, $f: ident) => {
+        impl $t for Rat {
+            type Output = Self;
+
+            fn $f(self, rhs: Self) -> Self {
+                Self(self.0.$f(rhs.0))
+            }
+        }
+    };
+}
+
+delegate_biop!(Add, add);
+delegate_biop!(Sub, sub);
+delegate_biop!(Mul, mul);
+delegate_biop!(Div, div);
+delegate_biop!(Rem, rem);
+
+impl Neg for Rat {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        Self(-self.0)
+    }
+}
+
+impl Num for Rat {
+    type FromStrRadixErr = ();
+
+    fn from_str_radix(s: &str, rad: u32) -> Result<Self, ()> {
+        if rad == 10 {
+            use std::str::FromStr;
+
+            Self::from_str(s)
+        } else {
+            Err(())
+        }
+    }
+}
+
+impl Zero for Rat {
+    fn zero() -> Self {
+        Self(BigRational::zero())
+    }
+
+    fn is_zero(&self) -> bool {
+        self.0.is_zero()
+    }
+
+    fn set_zero(&mut self) {
+        self.0.set_zero()
+    }
+}
+
+impl One for Rat {
+    fn one() -> Self {
+        Self(BigRational::one())
+    }
+
+    fn is_one(&self) -> bool {
+        self.0.is_one()
+    }
+
+    fn set_one(&mut self) {
+        self.0.set_one()
+    }
+}
+
+impl Pow::<Self> for Rat {
+    type Output = Self;
+
+    fn pow(self, rhs: Self) -> Self {
+        todo!()
+    }
+}
+
+impl std::fmt::Display for Rat {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
 }
