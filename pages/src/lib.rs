7d57657a7f1af3a02d89b9e9c3d471a1cc290055
funnsam <funnsam8@gmail.com>
2024-04-23 22:28:52 +0000

    lexx digits and workflow fix

--- pages/src/lib.rs
+++ pages/src/lib.rs
@@ -27,13 +27,13 @@ fn evaluate<T: ComputableNumeral + std::string::ToString>(s: &str) -> Result<T, 
 #[wasm_bindgen]
 pub fn evaluate_f32(s: &str) -> Result<String, JsSpan> {
     evaluate::<f32>(s)
-        .map(|a| trunc(&format!("{a:.6}")).to_string())
+        .map(|a| trunc(&format!("{a:.5}")).to_string())
 }
 
 #[wasm_bindgen]
 pub fn evaluate_f64(s: &str) -> Result<String, JsSpan> {
     evaluate::<f64>(s)
-        .map(|a| trunc(&format!("{a:.15}")).to_string())
+        .map(|a| trunc(&format!("{a:.13}")).to_string())
 }
 
 #[wasm_bindgen]
@@ -45,13 +45,13 @@ pub fn evaluate_rational(s: &str) -> Result<String, JsSpan> {
 #[wasm_bindgen]
 pub fn evaluate_cmplx_f32(s: &str) -> Result<String, JsSpan> {
     evaluate::<Complex<f32>>(s)
-        .map(|a| format!("{}+{}i", trunc(&format!("{:.6}", a.re)), trunc(&format!("{:.6}", a.im))))
+        .map(|a| format!("{}+{}i", trunc(&format!("{:.5}", a.re)), trunc(&format!("{:.5}", a.im))))
 }
 
 #[wasm_bindgen]
 pub fn evaluate_cmplx_f64(s: &str) -> Result<String, JsSpan> {
     evaluate::<Complex<f64>>(s)
-        .map(|a| format!("{}+{}i", trunc(&format!("{:.15}", a.re)), trunc(&format!("{:.15}", a.im))))
+        .map(|a| format!("{}+{}i", trunc(&format!("{:.13}", a.re)), trunc(&format!("{:.13}", a.im))))
 }
 
 // #[wasm_bindgen]
