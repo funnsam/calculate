ef1bb739139e5b013236133b644248517e8af73d
funnsam <funnsam8@gmail.com>
2024-04-28 02:00:02 +0000

    add eq sign

--- pages/src/lib.rs
+++ pages/src/lib.rs
@@ -38,6 +38,7 @@ fn evaluate<T: ComputableNumeral>(s: &str) -> Result<T, String> {
 pub fn evaluate_f32(s: &str) -> String {
     evaluate::<f32>(s)
         .map(|a| trunc(&format!("{a:.5}")).to_string())
+        .map(pretty_result)
         .unwrap_or_else(|s| s)
 }
 
@@ -45,6 +46,7 @@ pub fn evaluate_f32(s: &str) -> String {
 pub fn evaluate_f64(s: &str) -> String {
     evaluate::<f64>(s)
         .map(|a| trunc(&format!("{a:.13}")).to_string())
+        .map(pretty_result)
         .unwrap_or_else(|s| s)
 }
 
@@ -52,6 +54,7 @@ pub fn evaluate_f64(s: &str) -> String {
 pub fn evaluate_rational(s: &str) -> String {
     evaluate::<rational::Rational<BigInt>>(s)
         .map(|a| format!("{a:#}"))
+        .map(pretty_result)
         .unwrap_or_else(|s| s)
 }
 
@@ -59,6 +62,7 @@ pub fn evaluate_rational(s: &str) -> String {
 pub fn evaluate_cmplx_f32(s: &str) -> String {
     evaluate::<Complex<f32>>(s)
         .map(|a| pretty_cmplx(a, |a| format!("{a:.13}")))
+        .map(pretty_result)
         .unwrap_or_else(|s| s)
 }
 
@@ -66,6 +70,7 @@ pub fn evaluate_cmplx_f32(s: &str) -> String {
 pub fn evaluate_cmplx_f64(s: &str) -> String {
     evaluate::<Complex<f64>>(s)
         .map(|a| pretty_cmplx(a, |a| format!("{a:.13}")))
+        .map(pretty_result)
         .unwrap_or_else(|s| s)
 }
 
@@ -73,7 +78,12 @@ pub fn evaluate_cmplx_f64(s: &str) -> String {
 pub fn evaluate_cmplx_rational(s: &str) -> String {
     evaluate::<rational::complex::ComplexRational<BigInt>>(s)
         .map(|a| a.to_string())
+        .map(pretty_result)
         .unwrap_or_else(|s| s)
+}
+
+fn pretty_result(s: String) -> String {
+    format!("<span class=\"equals\">=</span> {}", sanitize(&s))
 }
 
 fn pretty_cmplx<T: num_traits::Float + num_traits::Signed, F: Fn(T) -> String>(c: Complex<T>, f: F) -> String {
